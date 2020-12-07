22c4fd3dca5fb1c56d0f927447f66d2caba9b08b
JunaidKurniawan <75241989+JunaidKurniawan@users.noreply.github.com>
2020-12-07 14:03:26 +0700

    Add files via upload

---
+++ data/Titikacuan_3.js
@@ -0,0 +1,9 @@
+var json_Titikacuan_3 = {
+"type": "FeatureCollection",
+"name": "Titikacuan_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.020766428575158, -7.428161299027302 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 111.020251241279084, -7.429949318373269 ] } }
+]
+}
